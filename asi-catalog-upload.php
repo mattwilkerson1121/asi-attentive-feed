e6fd98167de3ed2ea634ae99a4890e0d72860cc7
Matt Wilkerson <matt.wilkerson@americansignature.com>
2026-01-07 13:02:47 -0500

    Update asi-catalog-upload.php

--- asi-catalog-upload.php
+++ asi-catalog-upload.php
@@ -48,7 +48,7 @@
       <h1>ASI/Attentive Catalog Uploads</h1>
       <p>
         <?php 
-          $output = shell_exec("python catalog-upload.py asi-attentive-product-feed-xs.json --validateOnly true --apiKey {{ YOUR API KEY HERE!!! }}");
+          $output = shell_exec("python catalog-upload.py {{ FILE TO UPLOAD }} --validateOnly true --apiKey {{ YOUR API KEY HERE!!! }}");
           echo 'Response:' . $output; 
         ?>  
       </p>
